539fc1d370f926dd80d5896bc2943a1cf79dda3f
benhitchcock730 <blh9133@nyu.edu>
2024-04-11 15:21:54 -0400

    Creating asylum seeker map

--- js/pizza-data.js
+++ js/pizza-data.js
@@ -1,128 +1,128 @@
 var pizzaData = [
     {
-        "name": "Judy Huynh",
-        "pizza_restaurant_name": "L'Industrie Pizzeria",
-        "longitude": 73.9604494,
-        "latitude": 40.7116093,
-        "program": "MUP"
+        "name": "Floyd Bennet Field",
+        "pizza_restaurant_name": "currently houses about 2,000 migrants.",
+        "longitude": -73.88949330384084,
+        "latitude": 40.5911721677568,
+        "program": "Asylum Seeker Shelter"
     },
     {
-        "name": "Chris Whong",
-        "pizza_restaurant_name": "Ben's Pizza",
-        "longitude": -74.00017,
-        "latitude": 40.73046,
-        "program": "Professor"
+        "name": "JFK Airport Hangar",
+        "pizza_restaurant_name": "currently houses about 700 migrants.",
+        "longitude": -73.76935716147023,
+        "latitude": 40.65362364444828,
+        "program": "Asylum Seeker Shelter"
     },
     {
-        "name": "Sarah Ahmad",
-        "pizza_restaurant_name": "Nolita Pizza",
-        "longitude": -73.99618,
-        "latitude": 40.72091,
-        "program": "MUP"
+        "name": "The Roosevelt Hotel",
+        "pizza_restaurant_name": "currently houses about 3,100 migrants.",
+        "longitude": -73.97568893169839,
+        "latitude": 40.755190389854576,
+        "program": "Asylum Seeker Shelter"
     },
     {
-        "name": "Patrick Litke",
-        "pizza_restaurant_name": "Arturo's Coal Oven Pizza",
-        "longitude": -73.999787,
-        "latitude": 40.727537,
-        "program": "MSPP"
+        "name": "Beatstro",
+        "pizza_restaurant_name": "has provided more than 95,000 meals to asylum seekers so far.",
+        "longitude": -73.92707039162654,
+        "latitude": 40.80753274886378, 
+        "program": "Restaurant Partner"
     },
     {
-        "name": "Ceinna Little",
-        "pizza_restaurant_name": "Ben's Pizzaria",
-        "longitude": -74.00029,
-        "latitude": 40.73041,
-        "program": "MUP"
+        "name": "Brain Food",
+        "pizza_restaurant_name": "has provided more than 21,000 meals to asylum seekers so far.",
+        "longitude": -73.99118972046641,
+        "latitude": 40.69080464712702, 
+        "program": "Restaurant Partner"
     },
     {
-        "name": "Andrew DeFrank",
-        "pizza_restaurant_name": "Two Boots Pizza",
-        "longitude": -73.979088,
-        "latitude": 40.776428,
-        "program": "MUP"
+        "name": "Marlow Bistro",
+        "pizza_restaurant_name": "has provided more than 35,000 meals to asylum seekers so far.",
+        "longitude":  -73.96366285050132,
+        "latitude": 40.80320969545546,
+        "program": "Restaurant Partner"
     },
     {
-        "name": "Miranda H",
-        "pizza_restaurant_name": "Upside Pizza",
-        "longitude": -73.99275,
-        "latitude": 40.72434,
-        "program": "MUP"
+        "name": "Rethink HQ",
+        "pizza_restaurant_name": "is where the nonprofit's full time staff is located.",
+        "longitude": -74.01115533109368,
+        "latitude": 40.70500051637887, 
+        "program": "Rethink Food HQ"
     },
-    {
-        "name": "Camille Preel-Dumas",
-        "pizza_restaurant_name": "Pizzeria Panina",
-        "longitude": -73.90116,
-        "latitude": 40.7025,
-        "program": "MPA"
-    },
-    {
-        "name": "Eva Phillips",
-        "pizza_restaurant_name": "Brooklyn Pizza Crew",
-        "longitude": -73.95047368,
-        "latitude": 40.67317451,
-        "program": "MUP",
-    },
-    {
-        "name": "Marcos Rodriguez Anderson",
-        "pizza_restaurant_name": "Rosa's Pizza",
-        "longitude": -73.9001,
-        "latitude": 40.71217,
-        "program": "MUP"
-    },
-    {
-        "name": "Henry Kanengiser",
-        "pizza_restaurant_name": "L&B Spumoni Gardens",
-        "longitude": -73.98146,
-        "latitude": 40.59458,
-        "program": "MUP"
-    },
-    {
-        "name": "Mike Kriesberg",
-        "pizza_restaurant_name": "Bellucci's",
-        "longitude": -73.91549,
-        "latitude": 40.76412,
-        "program": "MPA"
-    },
-    {
-        "name": "Justin Quek",
-        "pizza_restaurant_name": "99 Cents Fresh Pizza",
-        "longitude": -73.98741,
-        "latitude": 40.75466,
-        "program": "MSPP"
-    },
-    {
-        "name": "Emily",
-        "pizza_restaurant_name": "Prospect Pizza Brooklyn",
-        "longitude": -73.96076862,
-        "latitude": 40.65890818,
-        "program": "MUP"
-    },
-    {
-        "name": "Noah",
-        "pizza_restaurant_name": "Mela Pizza",
-        "longitude": -73.92043079,
-        "latitude": 40.76767419,
-        "program": "MUP"
-    },
-    {
-        "name": "Mike",
-        "pizza_restaurant_name": "Andrew Belucci's",
-        "longitude": -73.91641811,
-        "latitude": 40.76440926,
-        "program": "MUP"
-    },
-    {
-        "name": "Yiyu Pan",
-        "pizza_restaurant_name": "Champion Pizza",
-        "longitude": -73.99747,
-        "latitude": 40.72164,
-        "program": "MSPP"
-    },
-    {
-        "name": "Ben Hitchcock",
-        "pizza_restaurant_name": "Zazzy's Pizza",
-        "longitude": -73.98858,
-        "latitude": 40.72158,
-        "program": "MPA"
-    }
 ]
+    // {
+    //     "name": "Camille Preel-Dumas",
+    //     "pizza_restaurant_name": "Pizzeria Panina",
+    //     "longitude": -73.90116,
+    //     "latitude": 40.7025,
+    //     "program": "MPA"
+    // },
+    // {
+    //     "name": "Eva Phillips",
+    //     "pizza_restaurant_name": "Brooklyn Pizza Crew",
+    //     "longitude": -73.95047368,
+    //     "latitude": 40.67317451,
+    //     "program": "MUP",
+    // },
+    // {
+    //     "name": "Marcos Rodriguez Anderson",
+    //     "pizza_restaurant_name": "Rosa's Pizza",
+    //     "longitude": -73.9001,
+    //     "latitude": 40.71217,
+    //     "program": "MUP"
+    // },
+    // {
+    //     "name": "Henry Kanengiser",
+    //     "pizza_restaurant_name": "L&B Spumoni Gardens",
+    //     "longitude": -73.98146,
+    //     "latitude": 40.59458,
+    //     "program": "MUP"
+    // },
+    // {
+    //     "name": "Mike Kriesberg",
+    //     "pizza_restaurant_name": "Bellucci's",
+    //     "longitude": -73.91549,
+    //     "latitude": 40.76412,
+    //     "program": "MPA"
+    // },
+    // {
+    //     "name": "Justin Quek",
+    //     "pizza_restaurant_name": "99 Cents Fresh Pizza",
+    //     "longitude": -73.98741,
+    //     "latitude": 40.75466,
+    //     "program": "MSPP"
+    // },
+    // {
+    //     "name": "Emily",
+    //     "pizza_restaurant_name": "Prospect Pizza Brooklyn",
+    //     "longitude": -73.96076862,
+    //     "latitude": 40.65890818,
+    //     "program": "MUP"
+    // },
+    // {
+    //     "name": "Noah",
+    //     "pizza_restaurant_name": "Mela Pizza",
+    //     "longitude": -73.92043079,
+    //     "latitude": 40.76767419,
+    //     "program": "MUP"
+    // },
+    // {
+    //     "name": "Mike",
+    //     "pizza_restaurant_name": "Andrew Belucci's",
+    //     "longitude": -73.91641811,
+    //     "latitude": 40.76440926,
+    //     "program": "MUP"
+    // },
+    // {
+    //     "name": "Yiyu Pan",
+    //     "pizza_restaurant_name": "Champion Pizza",
+    //     "longitude": -73.99747,
+    //     "latitude": 40.72164,
+    //     "program": "MSPP"
+    // },
+    // {
+    //     "name": "Floyd Bennet Field",
+    //     "pizza_restaurant_name": "Zazzy's Pizza",
+    //     "longitude": -73.98858,
+    //     "latitude": 40.72158,
+    //     "program": "MPA"
+    // }
--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,9 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiY3dob25nIiwiYSI6IjAyYzIwYTJjYTVhMzUxZTVkMzdmY
 
 var mapOptions = {
     container: 'map-container', // container ID
-    style: 'mapbox://styles/mapbox/dark-v11', // dark basemap
-    center: [-74.05, 40.68], // starting position [lng, lat]
-    zoom: 10.4, // starting zoom,
+    style: 'mapbox://styles/mapbox/light-v11', // dark basemap
+    center: [-73.91076292607373, 40.718914534163154,], // starting position [lng, lat]
+    zoom: 10.7, // starting zoom,
 }
 
 // instantiate the map
@@ -22,31 +22,38 @@ pizzaData.forEach(function (pizzaRecord) {
     var color
 
     // use if statements to assign colors based on pizzaData.program
-    if (pizzaRecord.program === 'Professor') {
+    if (pizzaRecord.program === 'Asylum Seeker Shelter') {
         color = '#8dd6a1'
     }
-    if (pizzaRecord.program === 'MUP') {
+    if (pizzaRecord.program === 'Rethink Food HQ') {
         color = '#d67ea6'
     }
-    if (pizzaRecord.program === 'MPA') {
-        color = '#1f8f39'
+    if (pizzaRecord.program === 'Restaurant Partner') {
+        color = '#0077b6'
     }
-    if (pizzaRecord.program === 'MSPP') {
-        color = '#8f5b1f'
-    }
+    
 
 
-    // create a popup to attach to the marker
+    // I will need 3 different types of pop-ups, as the information required for each type of site is different
+    //however I could not figure out how to do that (the if statement was not taking for some reason) so I sort of brute forced it.
     const popup = new mapboxgl.Popup({
         offset: 24,
         anchor: 'bottom'
-    }).setText(
-        `${pizzaRecord.name} loves to eat 🍕 at ${pizzaRecord.pizza_restaurant_name}`
+    }
+    ).setText(
+        `${pizzaRecord.name} ${pizzaRecord.pizza_restaurant_name}`
     );
+    
+    
+    
+    
+
+
+
 
     // create a marker, set the coordinates, add the popup, add it to the map
     new mapboxgl.Marker({
-        scale: 0.65,
+        scale: 0.8,
         color: color
     })
         .setLngLat([pizzaRecord.longitude, pizzaRecord.latitude])
